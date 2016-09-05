0c86f329ecfc6870b917aad38cab13d95ad160a4
BruceMellows <github@mellows.tv>
2016-09-05 15:11:07 +1000

    Added "no license" to sources
    Added "no license" to sources

--- NuPkg4Src/Utf8StringWriter.cs
+++ NuPkg4Src/Utf8StringWriter.cs
@@ -1,4 +1,10 @@
-﻿namespace NuPkg4Src
+﻿// NO LICENSE
+// ==========
+// There is no copyright, you can use and abuse this source without limit.
+// There is no warranty, you are responsible for the consequences of your use of this source.
+// There is no burden, you do not need to acknowledge this source in your use of this source.
+
+namespace NuPkg4Src
 {
     internal class Utf8StringWriter : System.IO.StringWriter
     {
